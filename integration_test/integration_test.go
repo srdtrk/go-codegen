5f13ac309bbdb048759c9692efd5c87f40365569
srdtrk <srdtrk@hotmail.com>
2024-02-13 00:57:36 -0500

    test: starting to add integration tests

---
+++ integration_test/integration_test.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"testing"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/suite"
+)
+
+type MySuite struct {
+	suite.Suite
+
+	logger       *zerolog.Logger
+	goCodegenDir string
+}
+
+func TestWithMySuite(t *testing.T) {
+	suite.Run(t, new(MySuite))
+}
+
+func (s *MySuite) SetupSuite() {
+	logger := zerolog.New(
+		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
+	).Level(zerolog.TraceLevel).With().Timestamp().Caller().Logger()
+
+	s.logger = &logger
+
+	s.goCodegenDir = "../build/go-codegen"
+}
+
+func (s *MySuite) GenerateGoCode(schemaDir string) {
+	// Generate Go code
+	// nolint:gosec
+	_, err := exec.Command(s.goCodegenDir, "generate", schemaDir).Output()
+	s.Require().NoError(err)
+}
+
+func (s *MySuite) GenerateGoCodeTestWithSchema(schemaDir string) {
+	s.GenerateGoCode(schemaDir)
+
+	// Run tests
+	// nolint:gosec
+	_, err := exec.Command("golangci-lint", "run", "output.go").Output()
+	s.Require().NoError(err)
+}
+
+func (s *MySuite) TestWithCwIcaController() {
+	s.GenerateGoCodeTestWithSchema("testdata/cw-ica-controller.json")
+}
