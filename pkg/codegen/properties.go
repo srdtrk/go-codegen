1a9d798f1b8361c96adc8ac655fa0f9f4352868a
srdtrk <59252793+srdtrk@users.noreply.github.com>
2024-04-22 08:53:16 +0700

    feat: add support for basic Tuples and Nullable types (#31)
    * test: added ics721 to test
    
    * feat: added basic fix for issue 30, still needs more work
    
    * imp: added json tuple marshal override
    
    * imp: custom marshaling done
    
    * refactor: using switch instead of if-else
    
    * feat: AnyOf generation added
    
    * test: adding new test
    
    * deps: ran 'go mod tidy'
    
    * fix: fixed json override
    
    * imp: fixed linter issue
    
    * fix: fix inline tuples of the same type

--- pkg/codegen/properties.go
+++ pkg/codegen/properties.go
@@ -43,28 +43,55 @@ func GenerateFieldFromSchema(name string, schema *schemas.JSONSchema, required *
 	return jen.Id(pascalName).Op(typeStr).Tag(tags)
 }
 
+// TODO: remove this function at v0.2.0 by breaking API
+func generateFieldsFromPropertiesWithoutTags(props map[string]*schemas.JSONSchema) []jen.Code {
+	fields := []jen.Code{}
+	for name, schema := range props {
+		// add comment
+		fields = append(fields, jen.Comment(schema.Description))
+		// add field
+		fields = append(fields, generateFieldFromSchemaWithoutTags(name, schema, nil, ""))
+	}
+	return fields
+}
+
+// TODO: remove this function at v0.2.0 by breaking API
+func generateFieldFromSchemaWithoutTags(name string, schema *schemas.JSONSchema, required *bool, typePrefix string) jen.Code {
+	if name == "" {
+		name = schema.Title
+	}
+	pascalName := strcase.ToCamel(name)
+
+	typeStr, err := getType(pascalName, schema, required, typePrefix)
+	if err != nil {
+		panic(err)
+	}
+
+	return jen.Id(pascalName).Op(typeStr)
+}
+
 func getType(name string, schema *schemas.JSONSchema, required *bool, typePrefix string) (string, error) {
 	if len(schema.Type) == 0 {
 		var underlyingSchemas []*schemas.JSONSchema
-		//nolint:gocritic //TODO(serdar): use switch
-		if schema.AllOf != nil {
+		switch {
+		case schema.AllOf != nil:
 			if len(schema.AllOf) > 1 {
 				return "", fmt.Errorf("length of allOf is greater than 1 in %s", name)
 			}
 			underlyingSchemas = schema.AllOf
-		} else if schema.AnyOf != nil {
+		case schema.AnyOf != nil:
 			if len(schema.AnyOf) > 2 {
 				return "", fmt.Errorf("length of anyOf is greater than 2 in %s", name)
 			}
 			underlyingSchemas = schema.AnyOf
-		} else if schema.Ref != nil {
+		case schema.Ref != nil:
 			if !strings.HasPrefix(*schema.Ref, defPrefix) {
 				return "", fmt.Errorf("cannot determine the type of %s", name)
 			}
 
 			typeStr := strings.TrimPrefix(*schema.Ref, defPrefix)
 			return typeStr, nil
-		} else {
+		default:
 			return "", fmt.Errorf("cannot determine the type of %s", name)
 		}
 
@@ -72,10 +99,7 @@ func getType(name string, schema *schemas.JSONSchema, required *bool, typePrefix
 			return "", fmt.Errorf("cannot determine the type of %s", name)
 		}
 
-		if underlyingSchemas[0].Ref == nil {
-			return "", fmt.Errorf("cannot determine the type of %s", name)
-		}
-		if len(*underlyingSchemas[0].Ref) == 0 {
+		if underlyingSchemas[0].Ref == nil || len(*underlyingSchemas[0].Ref) == 0 {
 			return "", fmt.Errorf("cannot determine the type of %s", name)
 		}
 
@@ -84,7 +108,7 @@ func getType(name string, schema *schemas.JSONSchema, required *bool, typePrefix
 			isOptional = !*required
 		} else {
 			isOptional = slices.ContainsFunc(underlyingSchemas, func(s *schemas.JSONSchema) bool {
-				return slices.Contains(s.Type, "null")
+				return slices.Contains(s.Type, schemas.TypeNameNull)
 			})
 		}
 
@@ -119,12 +143,40 @@ func getType(name string, schema *schemas.JSONSchema, required *bool, typePrefix
 	case schemas.TypeNameBoolean:
 		typeStr = "bool"
 	case schemas.TypeNameArray:
-		baseType, err := getType(schema.Title, &schema.Items[0], nil, "")
-		if err != nil {
-			return "", err
+		if typePrefix != "" {
+			return "", fmt.Errorf("cannot determine the type of array %s; type prefix is not supported", name)
 		}
 
-		typeStr = "[]" + baseType
+		switch {
+		case len(schema.Items) > 1 && !slices.ContainsFunc(schema.Items, func(s schemas.JSONSchema) bool { return len(s.Type) != 1 || s.Type[0] != schema.Items[0].Type[0] }):
+			// This case means that all items have the same type. This is similar to having an array of a single item.
+			fallthrough
+		case len(schema.Items) == 1 && schema.MaxItems == nil && schema.MinItems == nil:
+			baseType, err := getType(schema.Title, &schema.Items[0], nil, "")
+			if err != nil {
+				return "", err
+			}
+
+			typeStr = "[]" + baseType
+
+			isOptional = false // arrays are always nullable
+		case schema.MaxItems != nil && schema.MinItems != nil && *schema.MaxItems == *schema.MinItems && len(schema.Items) == *schema.MaxItems:
+			typeStr = "Tuple_of_"
+			for i := 0; i < *schema.MaxItems; i++ {
+				itemType, err := getType(name, &schema.Items[i], nil, "")
+				if err != nil {
+					return "", err
+				}
+
+				typeStr += itemType
+
+				if i < *schema.MaxItems-1 {
+					typeStr += "_and_"
+				}
+			}
+		default:
+			return "", fmt.Errorf("unsupported array definition %s", name)
+		}
 	case schemas.TypeNameObject:
 		switch {
 		case schema.Title != "":
