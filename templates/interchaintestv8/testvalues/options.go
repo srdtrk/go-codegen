226fc8ad35b2a1604c023d234fed775a1a7f855a
srdtrk <srdtrk@hotmail.com>
2024-05-21 01:19:11 +0300

    lint: linted

--- templates/interchaintestv8/testvalues/options.go
+++ templates/interchaintestv8/testvalues/options.go
@@ -5,11 +5,11 @@ import "github.com/gobuffalo/plush/v4"
 type Options struct{}
 
 // Validate that options are usable.
-func (opts *Options) Validate() error {
+func (*Options) Validate() error {
 	return nil
 }
 
-func (opts *Options) plushContext() *plush.Context {
+func (*Options) plushContext() *plush.Context {
 	ctx := plush.NewContext()
 	return ctx
 }
