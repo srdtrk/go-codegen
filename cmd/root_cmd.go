7b0585b0f229436fb46203c877ac7510daa62187
mahiro72 <mahiro.yoshie@gmail.com>
2024-06-18 00:15:40 +0900

    fix: deprecated message

--- cmd/root_cmd.go
+++ cmd/root_cmd.go
@@ -317,7 +317,7 @@ func ictestAddContract() *cobra.Command {
 
 	cmd.Flags().String(SuiteDirFlag, ".", "Path to the test suite directory. If not provided, the current working directory will be used.")
 	cmd.Flags().String(ContractNameFlag, "", "Name of the contract to be added to the test suite. If not provided, the contract name will be inferred from the schema file.")
-	cmd.Flags().MarkDeprecated(ContractNameFlag, "please use --package instead")
+	cmd.Flags().MarkDeprecated(ContractNameFlag, "please use --package-name instead")
 	cmd.Flags().StringP(PackageNameFlag, "p", "", "Package name for the generated contract code. If not provided, the contract name will be inferred from the schema file.")
 
 	return cmd
